db6dcdc673cf3b3ddb7d87ca20e356dc47372c22
vannsanng <vansang1526@gmail.com>
2022-06-27 09:32:25 +0700

    Add project files.

---
+++ PizzaPlace_2022/Shared/Spiciness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public enum Spiciness {
+        None,
+        Spicy,
+        Hot,
+        Camera,Canon,Nikon,Fujifilm
+    }
+}
